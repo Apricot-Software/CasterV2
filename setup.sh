a00ed525450cf288a66584d45f5183b8eda2777f
Limeade <116500605+LimeaidDev@users.noreply.github.com>
2024-08-08 02:51:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 sudo apt update
 
 # Install Python3 and venv if not already installed
-sudo apt install -y python3 python3-venv python3-pip gunicorn
+sudo apt install -y python3 python3-venv python3-pip
 
 # Create a virtual environment in the 'venv' directory
 python3 -m venv venv
